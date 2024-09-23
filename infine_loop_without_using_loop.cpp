d50de25d8a8bf0db7e96be25aae0fc4ad7ea3d9b
Pradeep Maurya <mauryapradeep814@gmail.com>
2024-09-23 12:45:56 +0530

    update code

--- infine_loop_without_using_loop.cpp
+++ infine_loop_without_using_loop.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+     int i = 2;
+      if(i%2==0){
+          label_1:cout<<i<<" if condi🔥"<<endl;
+          goto label_2;
+      }else{
+          label_2:cout<<i<<" else condi🔥"<<endl;
+          goto label_1;
+      }
+   //}
 
+    return 0;
+}
